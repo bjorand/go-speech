2e0f38c3746750c17c8f654c6206e63944185189
Benjamin Jorand <benjamin.jorand@gmail.com>
2018-03-01 22:44:02 +0100

    jupyter notebooks

---
+++ docs/jupyter-notebooks/README.md
@@ -0,0 +1,16 @@
+You can find here Jupyter notebooks of tests and POC code written in python.
+
+## Usage
+
+Create a `virtualenv` in this directory and install dependencies:
+```
+virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+Starts Jupyter:
+```
+jupyter-notebook
+```
+Go to the URL reported by the previous command and open on of the available notebook.
--- speechrec/brain.go
+++ speechrec/brain.go
@@ -56,34 +56,6 @@ func (b *Brain) Run() {
 		b.trainingWg.Add(1)
 		go b.trainWorker(i, b.Learn, &b.trainingWg)
 	}
-	// for i := 0; i < trainRoutines; i++ {
-	// 	b.trainingWg.Add(1)
-	// 	go func(i int) {
-	// 		fmt.Println("Start train routine", i)
-	// 		defer b.trainingWg.Done()
-	// 		defer fmt.Println("Stop train routine", i)
-	// 		for {
-	// 			select {
-	// 			case input, ok := <-b.Learn:
-	// 				if !ok {
-	// 					return
-	// 				}
-	// 				fmt.Printf("Learning with routine %d\n", i)
-	// 				s := time.Now()
-	//
-	// 				n := normalizeInput(input)
-	// 				b.M.Learn([][][]float64{
-	// 					{n, []float64{1.0}},
-	// 				})
-	// 				e := time.Now().Sub(s)
-	// 				fmt.Printf("Learning done by routine %d in %.3fs\n", i, float64(e.Nanoseconds())/1000000000.0)
-	//
-	// 			}
-	//
-	// 		}
-	// }(i)
-	// }
-
 }
 
 // Stop stops the brain
